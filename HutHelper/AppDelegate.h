f64e0f517ee4b8bdcba2bf2338880a98c00cca62
nine <245328522@qq.com>
2016-10-08 18:57:48 +0800

    Initial Commit

---
+++ HutHelper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HutHelper
+//
+//  Created by nine on 2016/10/8.
+//  Copyright © 2016年 nine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HutHelper/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HutHelper
+//
+//  Created by nine on 2016/10/8.
+//  Copyright © 2016年 nine. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
