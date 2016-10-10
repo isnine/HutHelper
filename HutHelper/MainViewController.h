45a93e94e3a642b1968e1287a836d7a8c4cda0e4
nine <245328522@qq.com>
2016-10-10 12:47:18 +0800

    主界面部分 - View层次完成

---
+++ HutHelper/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  HutHelper
+//
+//  Created by nine on 2016/10/10.
+//  Copyright © 2016年 nine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
---
+++ HutHelper/MainViewController.m
@@ -0,0 +1,37 @@
+//
+//  MainViewController.m
+//  HutHelper
+//
+//  Created by nine on 2016/10/10.
+//  Copyright © 2016年 nine. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
