433da33d1dcb5ee8f154db9195bfdd960d375e97
nine <245328522@qq.com>
2017-01-19 15:25:15 +0800

    -2

--- HutHelper/AddhandViewController.m
+++ HutHelper/AddhandViewController.m
@@ -9,7 +9,13 @@
 #import "AddhandViewController.h"
 
 @interface AddhandViewController ()
-
+@property (weak, nonatomic) IBOutlet UITextField *Title;
+@property (weak, nonatomic) IBOutlet UITextView *Describe;
+@property (weak, nonatomic) IBOutlet UITextField *Price;
+@property (weak, nonatomic) IBOutlet UITextField *Old;
+@property (weak, nonatomic) IBOutlet UITextField *Phone;
+@property (weak, nonatomic) IBOutlet UITextField *QQ;
+@property (weak, nonatomic) IBOutlet UITextField *Wechat;
 @end
 
 @implementation AddhandViewController
@@ -18,13 +24,31 @@
     [super viewDidLoad];
     self.navigationItem.title=@"发布商品";
     [self.navigationController.navigationBar setTitleTextAttributes:@{NSForegroundColorAttributeName:[UIColor blackColor]}];
-    // Do any additional setup after loading the view.
+    /**按钮*/
+    UIView *rightButtonView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 100, 50)];
+    UIButton *mainAndSearchBtn = [[UIButton alloc] initWithFrame:CGRectMake(70, 0, 50, 50)];
+    [rightButtonView addSubview:mainAndSearchBtn];
+    [mainAndSearchBtn setImage:[UIImage imageNamed:@"ok"] forState:UIControlStateNormal];
+    [mainAndSearchBtn addTarget:self action:@selector(postsay) forControlEvents:UIControlEventTouchUpInside];
+    UIBarButtonItem *rightCunstomButtonView = [[UIBarButtonItem alloc] initWithCustomView:rightButtonView];
+    self.navigationItem.rightBarButtonItem = rightCunstomButtonView;
+    _Describe.text = @"描述下你的商品...";
+    _Describe.textColor = [UIColor lightGrayColor];
+    _Describe.delegate=self;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
+-(BOOL)textViewShouldBeginEditing:(UITextView *)textView
+{
+    
+    textView.text=@"";
+    textView.textColor = [UIColor blackColor];
+    
+    return YES;
+    
+}
 
 @end
