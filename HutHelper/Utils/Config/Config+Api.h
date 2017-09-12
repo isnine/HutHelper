e6b92f4ffa08118eb04519deb62e7a7bf94ec9f3
Nine <245328522@qq.com>
2017-09-12 17:11:30 +0800

    FIX:更改appkey位置

--- HutHelper/Utils/Config/Config+Api.h
+++ HutHelper/Utils/Config/Config+Api.h
@@ -9,6 +9,17 @@
 #import "Config.h"
 
 @interface Config (Api)
+
+#define RONGCLOUD_APPKEY @"x4vkb1qpx05kk"
+#define APPKEY_UMESSAGE @"57fe13d867e58e0e59000ca1"
+#define APPKEY_QQ_SECRET @"y7n6BRLtnH9mrFT3"
+#define APPKEY_SINA_SECRET @"ba2997aaab6a1602406fc94247dc072d"
+#define APPKEY_WECHAT_SECRET @"8bb26c6a577e61f0bbee160dde7e79af"
+#define APPKEY_BMOB @"bb96c4df7fd649cc2eec6357242f9cc4"
+#define APPKEY_JSPATCH @"bd9208bd34ab8197"
+#define APPSTORE_ID 1164848835
+#define RSA_JSPATCH @"-----BEGIN PUBLIC KEY-----\nMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCi/XcpS7/lnAf0YEta273pubUy\niZGGy+xNOL2cV4XLpV0OmhCWunAdBuFSV7nn2HWcZWsTuRMt1gFUbO5gtFw6m2JH\niGvfK2YlQvRo91lGsbczad3SCe738lq6MiYNjyaoiCAW9U9+WxvX8DVUxzNhrlba\nwH1UzhSy5A8zWi8bMwIDAQAB\n-----END PUBLIC KEY-----"
+
 +(NSString*)getApiVedioShow;
 +(NSString*)getApiClass;
 +(NSString*)getApiClassXP;
