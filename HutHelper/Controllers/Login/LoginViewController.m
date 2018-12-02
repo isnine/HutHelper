43782cad77f1ce6d5af84d066ca4454c79832458
wangxiaozhen <wangxiaozhen@bytedance.com>
2018-12-02 14:02:13 +0800

    feat: 改为直接游客登录

--- HutHelper/Controllers/Login/LoginViewController.m
+++ HutHelper/Controllers/Login/LoginViewController.m
@@ -11,7 +11,7 @@
 #import "LeftSortsViewController.h"
 #import "JSONKit.h"
 #import "AppDelegate.h"
-  
+
 #import "MainPageViewController.h"
 #import "MBProgressHUD.h"
 #import "AFNetworking.h"
@@ -42,17 +42,17 @@
 
 - (IBAction)Login:(id)sender {
     if (_isEducation) {
-//        [Config saveEducationUserName:_UserName.text];
-//        [Config saveEducationPassWord:_Password.text];
+        //        [Config saveEducationUserName:_UserName.text];
+        //        [Config saveEducationPassWord:_Password.text];
         _userNameStr=_UserName.text;
         _passWorldStr=_Password.text;
-//        _userNameStr=@"2015143232";
-//        _passWorldStr=@"ys0132";
+        //        _userNameStr=@"2015143232";
+        //        _passWorldStr=@"ys0132";
         _secCode=_UtfSecCode.text;
-         [self acquireViewStare];
+        [self acquireViewStare];
         return;
     }
-     [self.view endEditing:YES];
+    [self.view endEditing:YES];
     NSString *UserName_String =[NSString stringWithFormat:@"%@",_UserName.text];
     NSString *Password_String =[NSString stringWithFormat:@"%@",_Password.text];
     [MBProgressHUD showMessage:@"登录中" toView:self.view];
@@ -63,114 +63,126 @@
     }
     [APIRequest GET:[Config getApiLogin:UserName_String passWord:Password_String] parameters:nil
             success:^(id responseObject) {
-                 HideAllHUD
-        NSDictionary *userAll = [NSDictionary dictionaryWithDictionary:responseObject];
-        NSString *msg=userAll[@"msg"];
-        if ([msg isEqualToString: @"ok"])
-        {
-            [Config saveUser:userAll[@"data"]];
-            [Config saveRememberCodeApp:userAll[@"remember_code_app"]];
-            [Config saveCurrentVersion:[[[NSBundle mainBundle] infoDictionary]
-                                        objectForKey:@"CFBundleShortVersionString"]];
-            [Config addNotice];
-            /**设置友盟标签&别名*/
-            [Config saveUmeng];
-            //如果是特殊用户
-            if (Config.getTrueName ==nil) {
-                [self.navigationController popToViewController:[self.navigationController.viewControllers objectAtIndex:([self.navigationController.viewControllers count] -2)] animated:YES];  //返回上一个View
-                return;
-            }
-            //请求即时聊天服务的Token
-                NSDictionary *dic=@{@"userid":Config.getUserId,
-                                    @"name":Config.getTrueName
-                                    };
-                [APIRequest POST:[Config getApiImToken] parameters:dic
-                         success:^(id responseObject) {
-                             NSDictionary *resultDictionary = [NSJSONSerialization JSONObjectWithData:responseObject options:NSJSONReadingMutableContainers error:nil];
-                             NSLog(@"%@",resultDictionary);
-                             [Config saveImToken:resultDictionary[@"token"]];
-//                             [[RCIM sharedRCIM] connectWithToken:[Config getImToken]
-//                                                         success:^(NSString *userId) {
-//                                                             NSLog(@"登陆成功。当前登录的用户ID：%@", userId);
-//                                                         } error:^(RCConnectErrorCode status) {
-//                                                             NSLog(@"私信模块登录错误，错误码:%ld");
-//                                                         } tokenIncorrect:^{
-//                                                             NSLog(@"Token错误,您无法使用私信功能,可尝试重新登录");
-//                                                         }];
-                         }
-                         failure:^(NSError *error) {
-                             NSLog(@"Token获取失败,您无法使用私信功能,可尝试重新登录");
-                         }];
-            //返回主界面
-            [self.navigationController popToViewController:[self.navigationController.viewControllers objectAtIndex:([self.navigationController.viewControllers count] -2)] animated:YES];  //返回上一个View
-        }
-        else {
-            NSString *Show_Msg=[msg stringByAppendingString:@",默认密码身份证后六位"];
-            if ([msg isEqualToString:@"多次失败，请稍后再试，或修改密码"]) {
-                UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"密码多次错误" message:msg preferredStyle:  UIAlertControllerStyleAlert];
-                [alert addAction:[UIAlertAction actionWithTitle:@"稍后再试" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
-                    //点击按钮的响应事件；
-                }]];
-                [alert addAction:[UIAlertAction actionWithTitle:@"修改密码" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
-                    UIStoryboard *mainStoryBoard              = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
-                    WebViewController *webViewController = [mainStoryBoard instantiateViewControllerWithIdentifier:@"Web"];
-                    webViewController.urlString=Config.getApiLoginReset;
-                    webViewController.viewTitle=@"重置密码";
-                    [self.navigationController pushViewController:webViewController animated:YES];
-                }]];
-                //弹出提示框；
-                [self presentViewController:alert animated:true completion:nil];
-            } else{
-                [MBProgressHUD showError:Show_Msg toView:self.view];
-            }
-            
-        }
-    }failure:^(NSError *error) {
-        HideAllHUD
-        [MBProgressHUD showError:@"网络错误或超时" toView:self.view];
-    }];
+                HideAllHUD
+                NSDictionary *userAll = [NSDictionary dictionaryWithDictionary:responseObject];
+                NSString *msg=userAll[@"msg"];
+                if ([msg isEqualToString: @"ok"])
+                {
+                    [Config saveUser:userAll[@"data"]];
+                    [Config saveRememberCodeApp:userAll[@"remember_code_app"]];
+                    [Config saveCurrentVersion:[[[NSBundle mainBundle] infoDictionary]
+                                                objectForKey:@"CFBundleShortVersionString"]];
+                    [Config addNotice];
+                    /**设置友盟标签&别名*/
+                    [Config saveUmeng];
+                    //如果是特殊用户
+                    if (Config.getTrueName ==nil) {
+                        [self.navigationController popToViewController:[self.navigationController.viewControllers objectAtIndex:([self.navigationController.viewControllers count] -2)] animated:YES];  //返回上一个View
+                        return;
+                    }
+                    //请求即时聊天服务的Token
+                    NSDictionary *dic=@{@"userid":Config.getUserId,
+                                        @"name":Config.getTrueName
+                                        };
+                    [APIRequest POST:[Config getApiImToken] parameters:dic
+                             success:^(id responseObject) {
+                                 NSDictionary *resultDictionary = [NSJSONSerialization JSONObjectWithData:responseObject options:NSJSONReadingMutableContainers error:nil];
+                                 NSLog(@"%@",resultDictionary);
+                                 [Config saveImToken:resultDictionary[@"token"]];
+                                 //                             [[RCIM sharedRCIM] connectWithToken:[Config getImToken]
+                                 //                                                         success:^(NSString *userId) {
+                                 //                                                             NSLog(@"登陆成功。当前登录的用户ID：%@", userId);
+                                 //                                                         } error:^(RCConnectErrorCode status) {
+                                 //                                                             NSLog(@"私信模块登录错误，错误码:%ld");
+                                 //                                                         } tokenIncorrect:^{
+                                 //                                                             NSLog(@"Token错误,您无法使用私信功能,可尝试重新登录");
+                                 //                                                         }];
+                             }
+                             failure:^(NSError *error) {
+                                 NSLog(@"Token获取失败,您无法使用私信功能,可尝试重新登录");
+                             }];
+                    //返回主界面
+                    [self.navigationController popToViewController:[self.navigationController.viewControllers objectAtIndex:([self.navigationController.viewControllers count] -2)] animated:YES];  //返回上一个View
+                }
+                else {
+                    NSString *Show_Msg=[msg stringByAppendingString:@",默认密码身份证后六位"];
+                    if ([msg isEqualToString:@"多次失败，请稍后再试，或修改密码"]) {
+                        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"密码多次错误" message:msg preferredStyle:  UIAlertControllerStyleAlert];
+                        [alert addAction:[UIAlertAction actionWithTitle:@"稍后再试" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+                            //点击按钮的响应事件；
+                        }]];
+                        [alert addAction:[UIAlertAction actionWithTitle:@"修改密码" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+                            UIStoryboard *mainStoryBoard              = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+                            WebViewController *webViewController = [mainStoryBoard instantiateViewControllerWithIdentifier:@"Web"];
+                            webViewController.urlString=Config.getApiLoginReset;
+                            webViewController.viewTitle=@"重置密码";
+                            [self.navigationController pushViewController:webViewController animated:YES];
+                        }]];
+                        //弹出提示框；
+                        [self presentViewController:alert animated:true completion:nil];
+                    } else{
+                        [MBProgressHUD showError:Show_Msg toView:self.view];
+                    }
+                    
+                }
+            }failure:^(NSError *error) {
+                HideAllHUD
+                [MBProgressHUD showError:@"网络错误或超时" toView:self.view];
+            }];
 }
 - (IBAction)touristBtn:(id)sender {
-    NSArray * str  = @[@"游客",@"湖南工业大学",@"江苏理工学院"];
-    zySheetPickerView *pickerView = [zySheetPickerView ZYSheetStringPickerWithTitle:str andHeadTitle:@"学校选择" Andcall:^(zySheetPickerView *pickerView, NSString *choiceString) {
-        if ([choiceString isEqualToString:@"游客"]) {
-            NSString *plistPath = [[NSBundle mainBundle]pathForResource:@"touristData" ofType:@"plist"];
-            NSDictionary *userAll = [[NSDictionary alloc] initWithContentsOfFile:plistPath];
-            NSLog(@"%@",userAll);
-            [Config saveUser:userAll[@"data"]];
-            [Config saveRememberCodeApp:userAll[@"remember_code_app"]];
-            [Config saveCurrentVersion:[[[NSBundle mainBundle] infoDictionary]
-                                        objectForKey:@"CFBundleShortVersionString"]];
-            [Config addNotice];
-            [Config saveUmeng];
-            [Config saveTourist:YES];
-            [self.navigationController popToViewController:[self.navigationController.viewControllers objectAtIndex:([self.navigationController.viewControllers count] -2)] animated:YES];  //返回上一个View
-        }else if ([choiceString isEqualToString:@"江苏理工学院"]){
-            [Config saveUrl:@"http://jwgl.jsut.edu.cn/(kerprnqgqy20pwmimmbrn3ax)"];
-            _isEducation=true;
-            _Password.placeholder=@"教务系统密码";
-            _UtfSecCode.placeholder=@"验证码";
-            _UtfSecCode.hidden=false;
-            _UivSecCode.hidden=false;
-            [_choiceBtn setTitle:@"江苏理工学院" forState: UIControlStateNormal];
-             [self refreshSecCode];
-        }else if ([choiceString isEqualToString:@"湖南工业大学"]){
-//            [Config saveUrl:@"http://218.75.197.123:83"];
-            [_choiceBtn setTitle:@"湖南工业大学" forState: UIControlStateNormal];
-            _isEducation=false;
-            _Password.placeholder=@"密 码";
-            _UtfSecCode.placeholder=@"验证码";
-            _UtfSecCode.hidden=true;
-            _UivSecCode.hidden=true;
-//            [self refreshSecCode];
-        }else{
-            
-        }
-
-        [pickerView dismissPicker];
-    }];
-    [pickerView show];
-    
+    NSString *plistPath = [[NSBundle mainBundle]pathForResource:@"touristData" ofType:@"plist"];
+    NSDictionary *userAll = [[NSDictionary alloc] initWithContentsOfFile:plistPath];
+    NSLog(@"%@",userAll);
+    [Config saveUser:userAll[@"data"]];
+    [Config saveRememberCodeApp:userAll[@"remember_code_app"]];
+    [Config saveCurrentVersion:[[[NSBundle mainBundle] infoDictionary]
+                                objectForKey:@"CFBundleShortVersionString"]];
+    [Config addNotice];
+    [Config saveUmeng];
+    [Config saveTourist:YES];
+    [self.navigationController popToViewController:[self.navigationController.viewControllers objectAtIndex:([self.navigationController.viewControllers count] -2)] animated:YES];  //返回上一个View
+    /**
+     NSArray * str  = @[@"游客",@"湖南工业大学",@"江苏理工学院"];
+     zySheetPickerView *pickerView = [zySheetPickerView ZYSheetStringPickerWithTitle:str andHeadTitle:@"学校选择" Andcall:^(zySheetPickerView *pickerView, NSString *choiceString) {
+     if ([choiceString isEqualToString:@"游客"]) {
+     NSString *plistPath = [[NSBundle mainBundle]pathForResource:@"touristData" ofType:@"plist"];
+     NSDictionary *userAll = [[NSDictionary alloc] initWithContentsOfFile:plistPath];
+     NSLog(@"%@",userAll);
+     [Config saveUser:userAll[@"data"]];
+     [Config saveRememberCodeApp:userAll[@"remember_code_app"]];
+     [Config saveCurrentVersion:[[[NSBundle mainBundle] infoDictionary]
+     objectForKey:@"CFBundleShortVersionString"]];
+     [Config addNotice];
+     [Config saveUmeng];
+     [Config saveTourist:YES];
+     [self.navigationController popToViewController:[self.navigationController.viewControllers objectAtIndex:([self.navigationController.viewControllers count] -2)] animated:YES];  //返回上一个View
+     }else if ([choiceString isEqualToString:@"江苏理工学院"]){
+     [Config saveUrl:@"http://jwgl.jsut.edu.cn/(kerprnqgqy20pwmimmbrn3ax)"];
+     _isEducation=true;
+     _Password.placeholder=@"教务系统密码";
+     _UtfSecCode.placeholder=@"验证码";
+     _UtfSecCode.hidden=false;
+     _UivSecCode.hidden=false;
+     [_choiceBtn setTitle:@"江苏理工学院" forState: UIControlStateNormal];
+     [self refreshSecCode];
+     }else if ([choiceString isEqualToString:@"湖南工业大学"]){
+     //            [Config saveUrl:@"http://218.75.197.123:83"];
+     [_choiceBtn setTitle:@"湖南工业大学" forState: UIControlStateNormal];
+     _isEducation=false;
+     _Password.placeholder=@"密 码";
+     _UtfSecCode.placeholder=@"验证码";
+     _UtfSecCode.hidden=true;
+     _UivSecCode.hidden=true;
+     //            [self refreshSecCode];
+     }else{
+     
+     }
+     
+     [pickerView dismissPicker];
+     }];
+     [pickerView show];
+     */
     
 }
 
@@ -323,10 +335,10 @@
             if (!error) {
                 self.UivSecCode.image = [UIImage imageWithData:responseObject];
             } else {
-               // [MBProgressHUD showMessage:@"验证码刷新失败" toView:self.view];
+                // [MBProgressHUD showMessage:@"验证码刷新失败" toView:self.view];
             }
         }] resume];
-
+        
     });
     
     
@@ -369,11 +381,11 @@
                       NSStringEncoding enc = CFStringConvertEncodingToNSStringEncoding (kCFStringEncodingGB_18030_2000);
                       NSData *data=responseObject;
                       NSString *tansData=[[NSString alloc]initWithData:data encoding:enc];
-//                      NSLog(@"biaodantijiaochenggong:%ld，%@",(long)operation.response.statusCode,operation.responseString);//提交表单
+                      //                      NSLog(@"biaodantijiaochenggong:%ld，%@",(long)operation.response.statusCode,operation.responseString);//提交表单
                       NSString *utf8HtmlStr = [tansData stringByReplacingOccurrencesOfString:@"<meta http-equiv=\"Content-Type\" content=\"text/html; charset=gb2312\">" withString:@"<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">"];
                       NSData *htmlDataUTF8 = [utf8HtmlStr dataUsingEncoding:NSUTF8StringEncoding];
                       [self anayseLoginData:htmlDataUTF8];
-                    //  [self.stepsController showNextStep];
+                      //  [self.stepsController showNextStep];
                   }
               } failure:^(NSURLSessionTask *operation, NSError *error) {
                   [self refreshSecCode];
