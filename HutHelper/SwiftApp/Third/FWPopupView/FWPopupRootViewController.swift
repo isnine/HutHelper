df2631cc011a3c7f26cbd9d2c769a147f3309f44
zhangchione <1525163730@qq.com>
2020-02-19 20:12:45 +0800

    fix:修复了iOS13bug

--- HutHelper/SwiftApp/Third/FWPopupView/FWPopupRootViewController.swift
+++ HutHelper/SwiftApp/Third/FWPopupView/FWPopupRootViewController.swift
@@ -12,11 +12,11 @@ import UIKit
 class FWPopupRootViewController: UIViewController {
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
-        if #available(iOS 13.0, *) {
-            return UIApplication.shared.windows.first?.windowScene?.statusBarManager?.statusBarStyle ?? UIStatusBarStyle.default
-        } else {
+//        if #available(iOS 13.0, *) {
+//            return UIApplication.shared.windows.first?.windowScene?.statusBarManager?.statusBarStyle ?? UIStatusBarStyle.default
+//        } else {
             return UIApplication.shared.statusBarStyle
-        }
+        //}
     }
     
     override var prefersStatusBarHidden: Bool {
